13a4d1a4e6f1cb07468f044eb88621c35046627c
Anshumna Diwakar <70698034+AnshumanD4@users.noreply.github.com>
2023-07-16 16:46:14 +0530

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -4,7 +4,7 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",player1);
 
 var random2 = (Math.floor(Math.random() * 6) +1);
-var player2 = "/images/dice" + random2 + ".png";
+var player2 = "images/dice" + random2 + ".png";
 var img2 = document.querySelectorAll("img")[1];
 img2.setAttribute("src", player2);
 
